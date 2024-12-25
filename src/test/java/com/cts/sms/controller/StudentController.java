61961f958263f10d3889fac8f33109e0b6601e6b
Suraj2282 <Suraj.Thorat@cognizant.com>
2024-12-25 14:27:06 +0530

    JWT Authentication Added

---
+++ src/test/java/com/cts/sms/controller/StudentController.java
@@ -0,0 +1,16 @@
+package com.cts.sms.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StudentController {
+
+	private StudentController studentController;
+	
+	@Test
+	public void addStudentToDatabase()
+	{
+		
+	}
+}
